74bfe7540a0c6748a0c870f367e666e52efc10e1
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-07-23 23:31:06 +0700

    feat: configure Laravel to trust all proxies for SSL termination

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -42,6 +42,8 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
